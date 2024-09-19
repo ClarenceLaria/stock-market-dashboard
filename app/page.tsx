2f9ae4eb42baf5bbdfe5413a5b743d033c35bf51
ClarenceLaria <clarencelaria@gmail.com>
2024-09-19 14:02:30 +0300

    Graph frame

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Header from "./Components/Header";
 import { FlagIcon } from '@heroicons/react/24/outline';
 import Sidebar from "./Components/SideBar";
+import pic from '@/public/images/bitcoin.png'
 
 export default function Home() {
   // const [data, setData] = useState<string | null>(null);
@@ -44,13 +45,21 @@ export default function Home() {
   // console.log(data);
   return (
     <main className="w-full h-full border-t-2 flex flex-row">
-      <div className='w-[46vh]'>
+      <div className='w-[56vh] mr-16'>
         <Sidebar></Sidebar>
       </div>
-      <div>
-        {/* <div className='w-full h-20 border-b-2'>
-          <h1 className='mx-4 text-6xl'>Dashboard</h1>
-        </div> */}
+      <div className='border-2 w-4/5 m-3 p-4'>
+        <h1 className='text-2xl font-bold mb-2'>Stock Market Value</h1>
+        <div className='flex flex-row justify-between text-lg'>
+          {/* <Image src={pic} alt='logo' width={1400} height={1400} className='w-10 h-10 my-1 mr-2'></Image> */}
+          <div className='flex flex-row'>
+            <h1 className='font-bold mr-2'>BTC</h1>
+            <h1>Bitcoin</h1>
+          </div>
+          <div className='justify-end'>
+            <h1 className='text-4xl font-bold'><span className='opacity-70 font-normal'>$</span>62,000</h1>
+          </div>
+        </div>
       </div>
     </main>
   );
